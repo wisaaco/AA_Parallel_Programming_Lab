52cf740d1d63d5b440d7f4c83f99b1ff7908c02f
Isaac.Lera <isaac.lera@gmail.com>
2022-11-07 14:31:36 +0100

    updates

---
+++ 02_Parallel_for/main.cpp
@@ -0,0 +1,31 @@
+#include <oneapi/tbb/info.h>
+#include <oneapi/tbb/parallel_for.h>
+#include <cassert>
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+// https://docs.oneapi.io/versions/latest/onetbb/tbb_userguide/Migration_Guide/Task_Scheduler_Init.html
+
+int main() {
+    // Get the default number of threads
+    int num_threads = oneapi::tbb::info::default_concurrency();
+    std::cout << "Default concurrency " << num_threads << std::endl;
+
+    auto values = std::vector<double>(10000);
+
+    oneapi::tbb::parallel_for( open::tbb::blocked_range<int>(0,values.size()),
+        [](tbb::blocked_range<int> r)
+        or (int i=r.begin(); i<r.end(); ++i)
+        {
+            values[i] = std::sin(i * 0.001);
+        }
+
+    double total = 0;
+    for (double value : values){
+        total += value;
+    }
+    
+    std::cout << "Total " << total << std::endl;
+    return 0;
+}
