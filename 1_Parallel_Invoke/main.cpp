5c8041afab5164397332d70e1fad0f2e01848f7a
Isaac.Lera <isaac.lera@gmail.com>
2022-11-08 12:25:44 +0100

    updates

--- 1_Parallel_Invoke/main.cpp
+++ 1_Parallel_Invoke/main.cpp
@@ -1,6 +1,7 @@
 //https://oneapi-src.github.io/oneAPI-spec/elements/oneTBB/source/algorithms/functions/parallel_invoke_func.html
 
 #include <iostream>
+#include <oneapi/tbb/info.h>
 #include "oneapi/tbb/parallel_invoke.h"
 
 int max(int num1, int num2);
@@ -18,15 +19,20 @@ int max(int num1, int num2) {
 
 
 int main(){
-    // https://www.intel.com/content/www/us/en/develop/documentation/advisor-user-guide/top/model-threading-designs/add-parallelism-to-your-program/replace-annotations-with-tbb-code/parallelize-functions-tbb-tasks.html
+
+   int num_threads = oneapi::tbb::info::default_concurrency();
+   std::cout << "Default concurrency " << num_threads << std::endl;
+
+
+   // https://www.intel.com/content/www/us/en/develop/documentation/advisor-user-guide/top/model-threading-designs/add-parallelism-to-your-program/replace-annotations-with-tbb-code/parallelize-functions-tbb-tasks.html
     
-    //https://en.cppreference.com/w/cpp/language/lambda
-    oneapi::tbb::parallel_invoke(
+   //https://en.cppreference.com/w/cpp/language/lambda
+   oneapi::tbb::parallel_invoke(
         [](){std::cout << "Hello " << std::endl;},
         [](){std::cout << "TBB " << std::endl;},
         [](){std::cout << max(10039,128903) << std::endl;},
         [&]{std::cout << "Otra tipo de llamada " <<std::endl;},
         [=]{std::cout << "Thanks! " <<std::endl;}
-        );
-    return 0;
+   );
+   return 0;
 }
