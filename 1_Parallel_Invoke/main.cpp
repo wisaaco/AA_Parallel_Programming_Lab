49a2680331df040ce2ed899917586f8f6281913f
Isaac.Lera <isaac.lera@gmail.com>
2022-11-08 10:30:56 +0100

    updates

--- 1_Parallel_Invoke/main.cpp
+++ 1_Parallel_Invoke/main.cpp
@@ -18,11 +18,22 @@ int max(int num1, int num2) {
 
 
 int main(){
+   // https://www.intel.com/content/www/us/en/develop/documentation/advisor-user-guide/top/model-threading-designs/add-parallelism-to-your-program/replace-annotations-with-tbb-code/parallelize-functions-tbb-tasks.html
+    
+    struct S1 { void operator()() { 
+         int result = 0;
+         if (result>=0){
+            std::cout << "Mayor que 0" << std::endl;
+         }
+     }};
+
     oneapi::tbb::parallel_invoke(
         [](){std::cout << "Hello " << std::endl;},
         [](){std::cout << "TBB " << std::endl;},
-        [](){std::cout << max(10039,128903) << std::endl;}
-        [&]{std::cout << "Otra tipo de llamada " <<std::endl;}
+        [](){std::cout << max(10039,128903) << std::endl;},
+        [&]{std::cout << "Otra tipo de llamada " <<std::endl;},
+        [=]{std::cout << "Thanks! " <<std::endl; },
+        S1()
     );
     return 0;
 }
